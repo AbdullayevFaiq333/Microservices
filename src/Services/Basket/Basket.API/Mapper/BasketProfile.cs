7604afe0803ed803654dc7ddb4d339a1d71bd811
faiq.abdullayev <faiq.abdullayev@RHD-PROG-7N>
2022-12-23 17:03:20 +0400

    RabbitMQ

---
+++ src/Services/Basket/Basket.API/Mapper/BasketProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Basket.API.Entities;
+using EventBus.Messages.Events;
+using System.Runtime.ConstrainedExecution;
+
+namespace Basket.API.Mapper
+{
+    public class BasketProfile : Profile
+    {
+        public BasketProfile()
+        {
+            CreateMap<BasketCheckout, BasketCheckoutEvent>().ReverseMap();
+        }
+    }
+}
